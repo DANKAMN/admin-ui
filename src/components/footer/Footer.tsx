03c25f06ba1c10ab2daca76b87614b1e71d34576
DANKAMN <danielnicholas1477@gmail.com>
2023-07-21 09:57:16 +0100

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span>supremeadmin</span>
+            <span>© Supreme Admin Dashboard</span>
+        </div>
+    )
+}
+
+export default Footer;
